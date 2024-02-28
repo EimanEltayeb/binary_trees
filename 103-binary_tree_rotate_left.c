917b71049775938d052f040d25ce59b45db9f98b
EimanEltayeb <em_moh092@hotmail.com>
2024-02-28 08:59:47 -0800

    null

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,6 +8,8 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *new_root;
 
+	if (tree == NULL)
+		return (NULL);
 	new_root = tree->right;
 	new_root->parent = NULL;
 	tree->parent = new_root;
