c8075e696424352a6514dcbb28baed4ad310899b
EimanEltayeb <em_moh092@hotmail.com>
2024-02-28 09:03:34 -0800

    rptate right

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_rotate_right - right-rotation on a binary tree
+ * @tree: pointer to root
+ * Return: new root pointer
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *new_root;
+
+	if (tree == NULL)
+		return (NULL);
+	new_root = tree->left;
+	new_root->parent = NULL;
+	tree->parent = new_root;
+	tree->left = new_root->right;
+	new_root->right = tree;
+	return (new_root);
+}
