18a94aad5b8d4bd3b54b469e05ea232e55968e5d
EimanEltayeb <140092109+EimanEltayeb@users.noreply.github.com>
2024-02-29 16:49:06 +0300

    Update 101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -6,24 +6,6 @@
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	binary_tree_t *sort[1000];
-	binary_tree_t *current;
-	int front = 0, rear = 0;
-
 	if (!tree || !func)
 		return;
-
-	sort[rear++] = (binary_tree_t *)tree;
-
-	while (front < rear)
-	{
-		current = sort[front++];
-		func(current->n);
-
-		if (current->left)
-			sort[rear++] = current->left;
-
-		if (current->right)
-			sort[rear++] = current->right;
-	}
 }
