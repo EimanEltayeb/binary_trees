9bbb452b62fcafeef9ce098aea123c1af054d9ec
EimanEltayeb <em_moh092@hotmail.com>
2024-02-29 11:03:14 -0800

    jhk

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -14,55 +14,59 @@ bst_t *bst_remove(bst_t *root, int value)
 	tmp = root;
 	while (tmp)
 	{
-		if (value == tmp->n)
-		{
-			if (!tmp->left && !tmp->right)
-			{
-				if (tmp->parent)
-				{
-
-			if (tmp->right->left)
-			{
-				new_tmp = tmp->right->left;
-				new_tmp->parent = tmp->parent;
-				new_tmp->left = tmp->left;
-				if (tmp->left)
-					tmp->left->parent = new_tmp;
-				new_tmp->right = tmp->right;
-				tmp->right->parent = new_tmp;
-				tmp->right->left = NULL;
-			}
-			else if (tmp->right)
-			{
-				new_tmp = tmp->right;
-				new_tmp->parent = tmp->parent;
-				new_tmp->left = tmp->left;
-				if (tmp->left)
-					tmp->left->parent = new_tmp;
-				if (tmp->parent->right == tmp)
-                                        tmp->parent->right = new_tmp;
-                                else
-                                        tmp->parent->left = new_tmp;
-			}
-			else
-			{
-				new_tmp = tmp->left;
-				if (tmp->left)
-					new_tmp->parent = tmp->parent;
-				if (tmp->parent->right == tmp)
-					tmp->parent->right = new_tmp;
-				else
-					tmp->parent->left = new_tmp;
-			}
-			free (tmp);
-			if (new_tmp->parent == NULL)
-				return (new_tmp);
-			return (root);
-		}
-		else if (value < tmp->n)
+		if (value < tmp->n)
 			tmp = tmp->left;
-		else
+		else if (value > tmp->n)
 			tmp = tmp->right;
+		else
+			break;
 	}
+	if (!tmp->right && !tmp->left)
+	{
+		if (tmp->parent->left == tmp)
+			tmp->parent->left = NULL;
+		else
+			tmp->parent->right = NULL;
+	}
+	else if (tmp->right)
+	{
+		if (tmp->right->left)
+		{
+			new_tmp = tmp->right->left;
+			new_tmp->parent = tmp->parent;
+			new_tmp->left = tmp->left;
+			if (tmp->left)
+				tmp->left->parent = new_tmp;
+			new_tmp->right = tmp->right;
+			tmp->right->parent = new_tmp;
+			tmp->right->left = NULL;
+		}
+		else
+		{
+			new_tmp = tmp->right;
+			new_tmp->parent = tmp->parent;
+			new_tmp->left = tmp->left;
+			if (tmp->left)
+				tmp->left->parent = new_tmp;
+			if (tmp->parent->right == tmp)
+				tmp->parent->right = new_tmp;
+			else
+				tmp->parent->left = new_tmp;
+		}
+	}
+	else
+	{
+		new_tmp = tmp->left;
+			if (tmp->left)
+				new_tmp->parent = tmp->parent;
+			if (tmp->parent->right == tmp)
+				tmp->parent->right = new_tmp;
+			else
+				tmp->parent->left = new_tmp;
+		}
+
+	free(tmp);
+	if (new_tmp->parent == NULL)
+		return (new_tmp);
 	return (root);
 }
