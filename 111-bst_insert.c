e49b1a57d5293c21a9bfd6966b39b377d4bfc33a
EimanEltayeb <em_moh092@hotmail.com>
2024-02-29 04:12:54 -0800

    fefds

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -8,6 +8,7 @@
 bst_t *bst_insert(bst_t **tree, int value)
 {
 	bst_t *tmp, *parent;
+
 	if (*tree == NULL)
 	{
 		*tree = binary_tree_node(NULL, value);
@@ -20,22 +21,22 @@ bst_t *bst_insert(bst_t **tree, int value)
 		parent = tmp;
 		if (value == tmp->n)
 			return (NULL);
-		if (value < tmp->n)
-		{
-			tmp = tmp->left;
-			if (tmp == NULL)
-			{
-				parent->left = binary_tree_node(parent, value);
-				return (parent->left);
-			}
-		}
-		else
+		if (value > tmp->n)
 		{
 			tmp = tmp->right;
 			if (tmp == NULL)
 			{
 				parent->right = binary_tree_node(parent, value);
 				return (parent->right);
+			}
+		}
+		else
+		{
+			tmp = tmp->left;
+			if (tmp == NULL)
+			{
+				parent->left = binary_tree_node(parent, value);
+				return (parent->left);
 			}
 		}
 	}
