b62551ec2bfb4102bc4d09e5e11685820fbb9cf2
EimanEltayeb <em_moh092@hotmail.com>
2024-02-29 10:17:08 -0800

    dgsdv

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -16,12 +16,18 @@ bst_t *bst_remove(bst_t *root, int value)
 	{
 		if (value == tmp->n)
 		{
+			if (!tmp->left && !tmp->right)
+			{
+				if (tmp->parent)
+				{
+
 			if (tmp->right->left)
 			{
 				new_tmp = tmp->right->left;
-				//new_tmp->parent = tmp->parent;
+				new_tmp->parent = tmp->parent;
 				new_tmp->left = tmp->left;
-				tmp->left->parent = new_tmp;
+				if (tmp->left)
+					tmp->left->parent = new_tmp;
 				new_tmp->right = tmp->right;
 				tmp->right->parent = new_tmp;
 				tmp->right->left = NULL;
@@ -29,25 +35,34 @@ bst_t *bst_remove(bst_t *root, int value)
 			else if (tmp->right)
 			{
 				new_tmp = tmp->right;
-
+				new_tmp->parent = tmp->parent;
+				new_tmp->left = tmp->left;
+				if (tmp->left)
+					tmp->left->parent = new_tmp;
+				if (tmp->parent->right == tmp)
+                                        tmp->parent->right = new_tmp;
+                                else
+                                        tmp->parent->left = new_tmp;
 			}
-			else if
+			else
 			{
-			}
-			new_tmp->parent = tmp->parent;
-
-
-
-
-
-
-			new_tmp = tmp->right;
-			new_tmp->parent = tmp->parent;
-			if (tmp->parent)
-			{
+				new_tmp = tmp->left;
+				if (tmp->left)
+					new_tmp->parent = tmp->parent;
 				if (tmp->parent->right == tmp)
 					tmp->parent->right = new_tmp;
 				else
 					tmp->parent->left = new_tmp;
 			}
-			new_tmp
+			free (tmp);
+			if (new_tmp->parent == NULL)
+				return (new_tmp);
+			return (root);
+		}
+		else if (value < tmp->n)
+			tmp = tmp->left;
+		else
+			tmp = tmp->right;
+	}
+	return (root);
+}
