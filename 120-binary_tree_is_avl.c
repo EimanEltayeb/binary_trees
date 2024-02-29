3795dcddc996c8c8e265ac35ed677fc86856e870
EimanEltayeb <em_moh092@hotmail.com>
2024-02-29 13:06:19 -0800

    gfv

---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_avl checks if a binary tree is a valid AVL Tree
+ * @tree: tree pointer
+ * Return: 0 or 1
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+}
---
+++ 121-avl_insert.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * array_to_avl - builds an AVL tree from an array
+ * @array: the array
+ * @size: size of array
+ * Return: the avl tree
+ */
+avl_t *array_to_avl(int *array, size_t size)
+{
+	if (!array)
+		return (NULL);
+}
