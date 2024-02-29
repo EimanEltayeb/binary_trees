40486f4271f7ccc7da313d6ee5d864d1f23991f8
EimanEltayeb <em_moh092@hotmail.com>
2024-02-29 04:05:37 -0800

    fgsr

---
+++ 111-bst_insert.c
@@ -0,0 +1,43 @@
+#include "binary_trees.h"
+/**
+ * bst_insert -  inserts a value in a Binary Search Tree
+ * @tree: pointer to root
+ * @value: value of the inserted node
+ * Return: pointer to the inserted node
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *tmp, *parent;
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
+	tmp = *tree;
+	parent = NULL;
+	while (1)
+	{
+		parent = tmp;
+		if (value == tmp->n)
+			return (NULL);
+		if (value < tmp->n)
+		{
+			tmp = tmp->left;
+			if (tmp == NULL)
+			{
+				parent->left = binary_tree_node(parent, value);
+				return (parent->left);
+			}
+		}
+		else
+		{
+			tmp = tmp->right;
+			if (tmp == NULL)
+			{
+				parent->right = binary_tree_node(parent, value);
+				return (parent->right);
+			}
+		}
+	}
+}
+
